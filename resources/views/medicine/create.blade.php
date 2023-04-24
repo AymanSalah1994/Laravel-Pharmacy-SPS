796811b29dbc150ddea818e1a5c68d95f6708661
Ayman Salah <ayman.1551.salah@gmail.com>
2023-04-24 18:55:03 +0200

    Added Medicine Views Folder

---
+++ resources/views/medicine/create.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    This is Create Medicine
+@endsection
+
+@section('scripts')
+@endsection
---
+++ resources/views/medicine/show.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    This is Show Medicine
+@endsection
+
+@section('scripts')
+@endsection
